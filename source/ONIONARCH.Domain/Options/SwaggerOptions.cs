82e02d27eb24ee55201843cc72aae4857ad437b0
Vercuski <lumaz@gmail.com>
2025-02-24 08:10:34 -0500

    Removed swagger Installed Scalar

--- source/ONIONARCH.Domain/Options/SwaggerOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿using ONIONARCH.Domain.Abstractions;
-
-namespace ONIONARCH.Domain.Options;
-
-public sealed record SwaggerOptions : BaseOptionsConfig
-{
-    public string ServerUrl { get; set; } = null!;
-    public override string Section => "Swagger";
-}
--- source/ONIONARCH.Presentation.API/Program.cs
+++ source/ONIONARCH.Presentation.API/Program.cs
@@ -1,26 +1,26 @@
 using ONIONARCH.Application;
 using ONIONARCH.Infrastructure;
 using ONIONARCH.Persistence;
-using ONIONARCH.Presentation.API.Swagger;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddOpenApi();
 builder.AddApplicationRegistration();
 builder.AddPersistenceRegistrations();
 builder.AddInfrastructureRegistration();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options => SwaggerGenOptionsConfiguration.ApplySwaggerGenOptions(options, builder));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.AddAppSwaggerConfiguration();
+    app.MapOpenApi();
 }
-
+app.MapScalarApiReference();
 app.MapControllers();
 app.AddInfrastructureApplicationRegistration();
 app.UseHttpsRedirection();
